651dbc8557541e46b905c7605629db62f0265ce5
raksha46kmt <69637713+raksha46kmt@users.noreply.github.com>
2020-12-06 10:26:42 +0530

    added event_details.js

--- Ajax/user_details.js
+++ Ajax/user_details.js
@@ -1,27 +1,28 @@
-function getUserName() {
+function getEventDetails() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-		console.log(this.responseText)
-     var event_list=JSON.parse(this.responseText).users;
-     var isUserFound=false;
-	 var ur_name= document.getElementById(user_name).value;
+    console.log(this.responseText)
+     var event_list=JSON.parse(this.responseText).events;
+     var isEventFound=false;
+   var event_name= document.getElementById("eventname").value;
      for(var i=0; i<event_list.length; i++)
      {
-     	if(ur_name==user_list[i].user_name)
-     	{
-     		var t= "<header><b>User Details</b><header><br/><table><tr><td>Name</td><td>"+user_list[i].name+"</td></tr><tr><td>Phone No</td><td>"+user_list[i].pho_no+"</td></tr><tr><td>Email id</td><td>"+user_list[i].email+"</td></tr><tr><td>Event</td><td>"+user_list[i].event+"</td></tr><tr><td>Venue</td><td>"+user_list[i].venue+"</td></tr><tr><td>Timimgs</td><td>"+user_list[i].timing+"</td></tr></table>"
-     		document.getElementById("user_name").innerHTML=t;
-     		isUserFound=true;
-     	}
+      if(event_name==event_list[i].eventname)
+      {
+        var t= "<header><b>Event Details</b><header><br/><table><tr><td>Name</td><td>"+event_list[i].name+"</td></tr><tr><td>Phone No</td><td>"+event_list[i].pho_no+"</td></tr><tr><td>Email id</td><td>"+event_list[i].email+"</td></tr><tr><td>Event</td><td>"+event_list[i].event+"</td></tr><tr><td>Venue</td><td>"+event_list[i].venue+"</td></tr><tr><td>Timimg</td><td>"+event_list[i].timing+"</td></tr></table>"
+        document.getElementById("event_details").innerHTML=t;
+        isEventFound=true;
+      }
      }
-     if (isUserFound==false)
+     if (isEventFound==false)
      {
-     	document.getElementById("user_name").innerHTML="user details not found";
+      document.getElementById("event_details").innerHTML="Event not found";
      }
      
     }
   };
-  xhttp.open("GET", "user_details.json", true);
+  xhttp.open("GET", "event_details.json", true);
   xhttp.send();
   }
+
