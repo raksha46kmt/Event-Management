a5f7a5b7c46a89dfbc9aa475d8387287ead2924d
raksha46kmt <69637713+raksha46kmt@users.noreply.github.com>
2020-12-06 10:57:23 +0530

    added event_details.js

--- Ajax/event_details.js
+++ Ajax/event_details.js
@@ -8,7 +8,7 @@ function getEventDetails() {
    var event_name= document.getElementById("eventname").value;
      for(var i=0; i<event_list.length; i++)
      {
-      if(event_name==event_list[i].eventname)
+      if(event_name==event_list[i].name)
       {
         var t= "<header><b>Event Details</b><header><br/><table><tr><td>Name</td><td>"+event_list[i].name+"</td></tr><tr><td>Phone No</td><td>"+event_list[i].pho_no+"</td></tr><tr><td>Email id</td><td>"+event_list[i].email+"</td></tr><tr><td>Event</td><td>"+event_list[i].event+"</td></tr><tr><td>Venue</td><td>"+event_list[i].venue+"</td></tr><tr><td>Timimg</td><td>"+event_list[i].timing+"</td></tr></table>"
         document.getElementById("event_details").innerHTML=t;
